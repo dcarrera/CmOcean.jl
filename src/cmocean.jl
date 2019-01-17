fc61cda63eb28d1ff777ae82277940fa59e3ad4f
Daniel Carrera <danielc@psu.edu>
2019-01-17 11:02:44 -0500

    Update for Julia v1.0
    This commit makes the script work on Julia v1.0. It also makes it easier to import the file, and it adds reversed ("_r") versions of all the colormaps.

--- src/cmocean.jl
+++ src/cmocean.jl
@@ -13,11 +13,19 @@ WARNING: This script overwrites the "gray" colormap from Matplotlib.
 
 =#
 using PyPlot
+using CSV
 
 for name in ["algae", "amp", "balance", "curl", "deep", "delta",
-			"dense", "gray", "haline", "ice", "matter", "oxy",
-			"phase", "solar", "speed", "tempo", "thermal", "turbid"]
-	cm = ColorMap(name, readdlm("rgb/$name-rgb.txt"))
+	"dense", "gray", "haline", "ice", "matter", "oxy",
+	"phase", "solar", "speed", "tempo", "thermal", "turbid"]
+	
+	path = "$(@__DIR__)/rgb/$name-rgb.txt"
+	df = CSV.read(path,header=false,delim=' ')
+	raw = convert(Array{Float64,2},df)
+	
+	cm = ColorMap(name, raw)
 	plt[:register_cmap](name=name,cmap=cm)
+	
+	cm = ColorMap(name*"_r", raw[end:-1:1,:])
+	plt[:register_cmap](name=name*"_r",cmap=cm)
 end
-
